003b2be33abbac1d9b69c294e86cb75a61646717
Abilka <51174445+Abilka@users.noreply.github.com>
2021-06-22 22:39:48 +0300

    Add files via upload

---
+++ mail.py
@@ -0,0 +1,62 @@
+import smtplib
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email import encoders
+
+import time
+import configparser
+
+
+
+
+
+def mail_send(filename: str, row_amount: int) -> None:
+    '''
+    :param email: gmail почта
+    :param password: пароль от почты выше
+    :param filename: (необязательно) файл, вложенный в письмо
+    '''
+
+    with open(filename, "rb") as attachment:
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+
+    part.add_header(
+        "Content-Disposition",
+        "attachment", filename=filename
+    )
+    encoders.encode_base64(part)
+    login_data = take_data_login_gmail()
+
+    message = MIMEMultipart()
+    message["From"] = login_data[0]
+    message["To"] = login_data[0]
+    message['Subject'] = "Курсы валют на " + str(time.asctime())
+
+    body = "В таблице " + str(row_amount) + " " + get_num_to_str(row_amount)
+
+    message.attach(MIMEText(body, "plain"))
+    message.attach(part)
+    text = message.as_string()
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(login_data[0], login_data[1])
+    server.sendmail(login_data[0], login_data[0], text.encode('utf-8'))
+    server.quit()
+
+
+def get_num_to_str(num: int) -> str:
+    lastNum = num % 10
+    if lastNum == 1:
+        return "строка"
+    elif lastNum in [2, 4, 5]:
+        return "строки"
+    else:
+        return "строк"
+
+
+def take_data_login_gmail() -> list:
+    setting = configparser.ConfigParser()
+    setting.read('mail.cfg')
+    return [setting.get('setting', "email"), setting.get('setting', "password")]
---
+++ main.py
@@ -0,0 +1,72 @@
+from selenium import webdriver
+import xlwt
+import threading
+import mail
+
+
+class Browser:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+
+    def take_course_yandex(self, button_class_name: str, money_name: str) -> None:
+        self.driver.get('https://yandex.ru/')
+
+        self.driver.find_element_by_xpath(f'//*[@class="{button_class_name}"]/a').click()
+        self.driver.switch_to.window(self.driver.window_handles[-1])
+        course = self.driver.find_element_by_xpath('//*[@class="news-stock-table__content"]')
+
+        self.money_name = money_name
+        value[self.money_name] = []
+
+        for course_line in course.find_elements_by_tag_name('div'):
+            course_line_info = course_line.find_elements_by_tag_name('div')
+
+            if len(course_line_info) > 2:
+                if course_line_info[0].text != 'Дата':
+                    value[self.money_name].append({'date': course_line_info[0].text,
+                                                   'course': course_line_info[1].text,
+                                                   'change': course_line_info[2].text})
+
+
+class Exel:
+    def __init__(self):
+        self.wb = xlwt.Workbook()
+        self.ws = self.wb.add_sheet('Курсы валют')
+        self.row = 0
+        self.col = 0
+        self.all_amount_row = 0
+
+    def create_course(self):
+        for cur_name in value:
+            for string in value[cur_name]:
+                style = xlwt.XFStyle()
+                style.num_format_str = '[$R]#,##0.00'
+                self.ws.write(self.row, self.col, string["date"], style)
+                self.ws.write(self.row, self.col + 1, string["course"], style)
+                self.ws.write(self.row, self.col + 2, string["change"], style)
+                self.row += 1
+                self.all_amount_row = self.row
+            self.col += 3
+            self.row = 0
+        self.divison()
+
+    def divison(self):
+        for row in range(1, 11):
+            style = xlwt.XFStyle()
+            style.num_format_str = '0.00'
+            self.ws.write(row-1, self.col, xlwt.Formula(f'$B{row}/$E{row}'), style)
+        self.wb.save('Course.xls')
+
+
+
+if __name__ == '__main__':
+    value = {}
+    threading.Thread(target=Browser().take_course_yandex, args=(
+        'b-inline inline-stocks__item inline-stocks__item_id_2002 hint__item inline-stocks__part', 'dollar')).start()
+    Browser().take_course_yandex(
+        'b-inline inline-stocks__item inline-stocks__item_id_2000 hint__item inline-stocks__part',
+        'euro')
+
+    courseFile = Exel()
+    courseFile.create_course()
+    mail.mail_send("Course.xls", courseFile.all_amount_row)
